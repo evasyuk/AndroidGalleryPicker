c9f86872b3b63c5db86ea4db2c102d68d1d8d086
dp050193veo <dp050193veo@gitlab.localhost>
2015-08-30 15:53:00 +0300

    license added to all the files

--- app/src/main/java/ua/pb/gallery/adapters/GenericAdapter.java
+++ app/src/main/java/ua/pb/gallery/adapters/GenericAdapter.java
@@ -27,6 +27,24 @@ import ua.pb.gallery.Utils;
 import ua.pb.gallery.models.FileItemModel;
 
 /**
+
+ Copyright 2015 evasyuk  < vasyuk.eugene@gmail.com >
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ */
+
+/**
  * Created by user on 29.08.15.
  */
 public class GenericAdapter extends RecyclerView.Adapter<GenericAdapter.FileItemHolderView> implements FilterInterface{
